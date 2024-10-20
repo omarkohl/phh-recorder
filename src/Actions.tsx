fb8f07506b1bbb10af3e5d26fe1504300155d26d
Omar Kohl <omarkohl@posteo.net>
2024-10-20 14:32:18 +0200

    feat: improve visual style of actor selector combobox
    Leaning heavily on the example combobox from
    https://headlessui.com/react/combobox

--- src/Actions.tsx
+++ src/Actions.tsx
@@ -2,7 +2,9 @@ import {useState} from 'react';
 import {Actor, DEALER, getDisplayName, isPlayer, Player} from './Player';
 import Action from "./Action.ts";
 
-import {Combobox, ComboboxInput, ComboboxOption, ComboboxOptions} from '@headlessui/react';
+import {Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions} from '@headlessui/react';
+import clsx from "clsx";
+import {CheckIcon, ChevronDownIcon} from "@heroicons/react/20/solid";
 
 function Actions(
     props: Readonly<{
@@ -60,20 +62,36 @@ function Actions(
                     value={currentActor}
                     onClose={() => setQuery('')}
                 >
-                    <ComboboxInput
-                        placeholder="Choose an actor"
-                        displayValue={getDisplayName}
-                        onChange={(event) => setQuery(event.target.value)}
-                    />
-                    <ComboboxOptions anchor="bottom start"
-                                     className="w-[var(--input-width)] border empty:invisible bg-white">
+                    <div className="relative">
+                        <ComboboxInput
+                            placeholder="Choose an actor"
+                            displayValue={getDisplayName}
+                            className={clsx(
+                                'w-full rounded-lg border-none bg-gray-100 py-1.5 pr-8 pl-3 text-sm/6 text-black',
+                                'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                            )}
+                            onChange={(event) => setQuery(event.target.value)}
+                        />
+                        <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
+                            <ChevronDownIcon className="size-4 fill-gray-600 group-data-[hover]:fill-black"/>
+                        </ComboboxButton>
+                    </div>
+                    <ComboboxOptions
+                        anchor="bottom"
+                        transition
+                        className={clsx(
+                            'w-[var(--input-width)] rounded-xl border border-gray-300 bg-gray-100 p-1 [--anchor-gap:var(--spacing-1)] empty:invisible',
+                            'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0'
+                        )}
+                    >
                         {filteredPlayers.map(player => (
                             <ComboboxOption
                                 key={player.id}
                                 value={player}
-                                className="data-[focus]:bg-blue-100"
+                                className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-gray-200"
                             >
-                                {getDisplayName(player)}
+                                <CheckIcon className="invisible size-4 fill-gray-600 group-data-[selected]:visible"/>
+                                <div className="text-sm/6 text-black">{getDisplayName(player)}</div>
                             </ComboboxOption>
                         ))}
                     </ComboboxOptions>
